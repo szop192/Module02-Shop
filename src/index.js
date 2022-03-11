41cbb00ae253e34b0f92ff7069f2aa5b82ea2cb5
szop192 <97298754+szop192@users.noreply.github.com>
2022-03-11 11:01:31 +0300

    add all

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import 'normalize.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import './styles/main.scss'
 import Swiper, { Navigation, Pagination } from 'swiper';
+import './styles/main.scss'
 
  var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+});
+
+var swiper = new Swiper(".trendsSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
